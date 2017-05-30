62208976cb6da3318ac0809f48cca7452ac12708
dorian821 <arainboldt@gmail.com>
2017-05-30 10:18:36 +0300

    Update trendy_turny.py

--- trendy_turny.py
+++ trendy_turny.py
@@ -1,1 +1,17 @@
+stackoverflow.com/questions/8587047/support-resistance-algorithm-technical-analysis+&cd=3&hl=en&ct=clnk&gl=gr
+def turningpoints(lst):
+    dx = np.diff(lst)
+    return np.sum(dx[1:] * dx[:-1] < 0)
 
+
+#trend or turn
+def trend_turn(data):
+  time_frame = (5,10,20)
+  for t in time_frame:
+    sma = data['Typical'].rolling(window=t).apply(mean)
+    slope = sma.rolling(window=3).apply(three_linest)
+    data[str(t) + 'd_Trend/Turn'][slope > highband] = 'Up_Trend'
+    data[str(t) + 'd_Trend/Turn'][slope < lowband] = 'Down_Trend'
+    data[str(t) + 'd_Trend/Turn'][(data[str(t) + 'd_Trend/Turn'].shift(-1) == 'Down_Trend') & (data[str(t) + 'd_Trend/Turn'].isnull())] = 'Down_Turn'
+    data[str(t) + 'd_Trend/Turn'][(data[str(t) + 'd_Trend/Turn'].shift(-1) == 'Up_Trend') & (data[str(t) + 'd_Trend/Turn'].isnull())] = 'Up_Turn'
+    data[str(t) + 'd_Trend/Turn'].fillna(method='bfill')
