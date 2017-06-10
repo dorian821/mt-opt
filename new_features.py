ba0519e9f74595400080e69088656ce883d14291
dorian821 <arainboldt@gmail.com>
2017-06-10 18:32:15 +0300

    Create new_features.py

--- new_features.py
+++ new_features.py
@@ -16,3 +16,12 @@ def vwap_ratios(data):
     data = data.assign('D1Lo/VWAP'=data['low']/data['VWAP'])
     return data
     
+def support_resistance(data):
+    cluster highs and lows by normalized proximity
+    take average of each cluster
+    store in dictionary with number of pop
+    for each day print nearest support and resistance and ratio of low/high to these
+    return nearest support and resistance and ratios
+    
+    
+    
