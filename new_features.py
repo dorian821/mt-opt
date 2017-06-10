dc417ec40b97f625995f187fb9faf05599bc85d9
dorian821 <arainboldt@gmail.com>
2017-06-10 17:49:39 +0300

    Create new_features.py

--- new_features.py
+++ new_features.py
@@ -3,6 +3,16 @@ from numba import jit
 #VWAP
 @jit
 def vwap(data):
-  vwap = pd.Series(data=np.cumsum(v*(h+l)/2) / np.cumsum(v),index=data.index,name='VWAP')
-  data = data.join(vwap)
+  data = data.assign('VWAP'=np.cumsum(data['Volume']*data['Typical'])/ np.cumsum(data['Volume']))
   return data
+
+
+#VWAP Ratios
+
+def vwap_ratios(data):
+    data = data.assign('VWAP_3dSlope'=data['VWAP'].rolling(window=3,center=False).apply(three_linest))
+    data = data.assign('5d_SMA/VWAP'=data['5d_SMA']/data['VWAP'])
+    data = data.assign('D1Hi/VWAP'=data['High']/data['VWAP'])
+    data = data.assign('D1Lo/VWAP'=data['low']/data['VWAP'])
+    return data
+    
