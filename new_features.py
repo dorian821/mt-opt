4c5fd115623cc49a4d0e344ece9d6fdea932f3b8
dorian821 <arainboldt@gmail.com>
2017-06-11 17:04:44 +0300

    Create new_features.py

--- new_features.py
+++ new_features.py
@@ -3,49 +3,68 @@ from numba import jit
 #VWAP
 @jit
 def vwap(data):
-  data.assign('VWAP'=np.cumsum(data['Volume']*data['Typical'])/ np.cumsum(data['Volume']),inplace=True)
-  return data
+    data = data.assign(VWAP=np.cumsum(data['Volume']*data['Typical'])/np.cumsum(data['Volume']))
+    return data
 
+def typ(data):
+    data = data.join(pd.Series(data=(data['High']+data['Low']+data['Close'])/3,index=data.index,name='Typical'))
+    return data
 
 #VWAP Ratios
-
 def vwap_ratios(data):
-    data.assign('VWAP_3dSlope'=data['VWAP'].rolling(window=3,center=False).apply(three_linest),inplace=True)
-    data.assign('5d_SMA/VWAP'=data['5d_SMA']/data['VWAP'],inplace=True)
-    data.assign('D1Hi/VWAP'=data['High']/data['VWAP'],inplace=True)
-    data.assign('D1Lo/VWAP'=data['low']/data['VWAP'],inplace=True)
+    data.assign(VWAP_3dSlope=data['VWAP'].rolling(window=3,center=False).apply(three_linest),inplace=True)
+    data = data.join(pd.Series(data=data['5d_SMA']/data['VWAP'],index=data.index,name='5d_SMA/VWAP'))
+    data = data.join(pd.Series(data=data['High']/data['VWAP'],index=data.index,name='5d_SMA/VWAP'))
+    data = data.join(pd.Series(data=data['Low']/data['VWAP'],index=data.index,name='5d_SMA/VWAP'))
     return data
+
   
-  
-def nearest_resup(val,arr,direction)
+def nearest_resup(val,arr,direction):
     idx = np.argmin(np.abs(arr-val))
     return arr.ix[idx+direction]
     
 def support_resistance(data,margin,level):    
-    acres = np.sort(data[['High','Low']].stack())
-    while (acres != centers) | (len(centers)):
-      acres = acres1
-      centers = []
-      for i in acres:
-          diff = np.abs(acres[i] - acres)/acres
-          cluster = diff[diff<margin]
-          strength = len(cluster)
-          if strength>=level:
-              centers.extend(cluster.mean())
-      acres1 = centers
+    acres = list(np.sort(data[['High','Low']].stack()))
+    centers = []
+    acres1 = []    
+    m, n = 1, 0
+    while n != m:
+        print(n,m)
+        m = len(centers)
+        centers = []
+        for i,value in enumerate(acres):
+            diff = list(np.abs(acres[i] - acres)/acres)
+            cluster = [x for (x,y) in zip(acres,diff) if y < margin] #acres[diff<margin]
+            strength = len(cluster)
+            if strength>=level:
+                centers.extend([np.sum(cluster)/strength])
+                n = len(centers)
+        acres = centers
     centers = np.sort(centers)
     for j in data.index:
         data.loc[j,'Resistance'] = nearest_resup(data.loc[j]['High'],centers,1)
         data.loc[j,'Support'] = nearest_resup(data.loc[j]['High'],centers,-1)        
-    data.assign('Resistance_Ratio'=data['High']/data['Resistance'],inplace=True)
-    data.assign('Support_Ratio'=data['Low']/data['Resistance'],inplace=True)
-    return centers 
-            
+    data = data.join(pd.Series(data=data['High']/data['Resistance'],index=data.index,name='Resistance_Ratio'))
+    data = data.join(pd.Series(data=data['Low']/data['Support'],index=data.index,name='Support_Ratio'))
+    return data 
+
     cluster highs and lows by normalized proximity
     take average of each cluster
     store in dictionary with number of pop
     for each day print nearest support and resistance and ratio of low/high to these
     return nearest support and resistance and ratios
-    
+  
+  
+for i in acres.index:
+    diff = pd.Series(np.abs(acres[i] - acres)/acres)
+    #print('took diffs')
+    cluster = acres[diff<margin]
+    #print('clustered')
+    strength = len(cluster)
+    #print('strength measured')
+    if strength>=level:
+        #print('list extending')
+        centers.extend([cluster.mean()])
+        
     
     
