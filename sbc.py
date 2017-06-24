b13d2d0baacdd9ac2475b5f9d8869cff1dba8b8e
dorian821 <arainboldt@gmail.com>
2017-06-24 08:59:28 +0300

    Update sbc.py

--- sbc.py
+++ sbc.py
@@ -1,1 +1,22 @@
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import mutual_info_classif
+from sklearn.feature_selection import RFECV
+from sklearn.feature_selection import VarianceThreshold
+from random import randint
 
+x_data = SelectKBest(mutual_info_classif, k=randint(3,20)).fit_transform(x_data, y)
+
+def scorer(estimator, X, y):
+  mask = (prediction == 0) | (actual == prediction)
+  return mask.sum()/len(prediction)
+
+model = GaussianNB()
+def lowvar_clip(model,data,target):
+   selection = RFECV(model, step=1, scoring=scorer, cv=randint(3,20),n_jobs=4).fit(data,target)
+   return selection
+
+def var_check(data,threshold=0.0):
+  selector = VarianceThreshold(threshold=0.0)
+  selection = selector.fit_transform(data)
+  return selection
+    
